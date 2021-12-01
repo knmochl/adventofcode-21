e6cf640c83251db490276b3be56ba0a562e1ebc3
Nathan Sullivan <nsullivan@netsuite.com>
2021-11-30 23:00:24 -0800

    Add solution for Day 1 Part 1

--- src/advent/day1.clj
+++ src/advent/day1.clj
@@ -1,5 +1,6 @@
 (ns advent.day1
-  (:require [advent.util :refer [slurp-lines]]))
+  (:require [advent.util :refer [slurp-lines]]
+            [clojure.edn :as edn]))
 
 (def test-input
   ["199"
@@ -12,3 +13,14 @@
    "269"
    "260"
    "263"])
+
+(defn depth-reducer
+  [[increases previous] new-value]
+  (if (> new-value previous)
+    [(inc increases) new-value]
+    [increases new-value]))
+
+(defn part1
+  []
+  (let [depths (mapv edn/read-string (slurp-lines "input1.txt"))]
+    (first (reduce depth-reducer [0 (first depths)] (rest depths)))))
