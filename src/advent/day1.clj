b1f0b4d0efd7f92fc7e3331abdd12f2d7c208f6b
Nathan Sullivan <nsullivan@netsuite.com>
2021-11-30 22:34:21 -0800

    Add test-input for Day 1

---
+++ src/advent/day1.clj
@@ -0,0 +1,14 @@
+(ns advent.day1
+  (:require [advent.util :refer [slurp-lines]]))
+
+(def test-input
+  ["199"
+   "200"
+   "208"
+   "210"
+   "200"
+   "207"
+   "240"
+   "269"
+   "260"
+   "263"])
