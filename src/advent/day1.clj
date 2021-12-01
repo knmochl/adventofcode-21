7db9060da75f33a5c5f77934bd4c3c2365db755c
Nathan Sullivan <nsullivan@netsuite.com>
2021-11-30 23:00:57 -0800

    Add solution for Day 1 Part 2

--- src/advent/day1.clj
+++ src/advent/day1.clj
@@ -20,7 +20,20 @@
     [(inc increases) new-value]
     [increases new-value]))
 
+(defn depth-sliding-reducer
+  [[increases previous1 previous2 previous3] new-value]
+  (if (> (+ new-value previous3 previous2) (+ previous1 previous2 previous3))
+    [(inc increases) previous2 previous3 new-value]
+    [increases previous2 previous3 new-value]))
+
 (defn part1
   []
   (let [depths (mapv edn/read-string (slurp-lines "input1.txt"))]
     (first (reduce depth-reducer [0 (first depths)] (rest depths)))))
+
+(defn part2
+  []
+  (let [depths (mapv edn/read-string (slurp-lines "input1.txt"))
+        [num1 num2 num3] (take 3 depths)
+        depths-remaining (drop 3 depths)]
+    (first (reduce depth-sliding-reducer [0 num1 num2 num3] depths-remaining))))
