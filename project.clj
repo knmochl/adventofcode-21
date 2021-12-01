dccefe56ddbd81f3d1ba1415be9e0b97983e969e
Nathan Sullivan <nsullivan@netsuite.com>
2021-11-30 20:15:27 -0800

    Update project file

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject adventofcode-21 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject advent "0.1.0-SNAPSHOT"
+  :description "Advent Of Code 2021"
+  :url "https://github.com/knmochl/adventofcode-21"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :main ^:skip-aot adventofcode-21.core
+  :main ^:skip-aot advent.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
