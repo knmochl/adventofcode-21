83117f9b90ed3444f95e9395b8e7edffc9ca3661
Nathan Sullivan <nsullivan@netsuite.com>
2021-12-01 22:27:51 -0800

    Add solution for Day 2 Part 1

--- src/advent/day2.clj
+++ src/advent/day2.clj
@@ -1,6 +1,6 @@
 (ns advent.day2
   (:require [advent.util :refer [slurp-lines]]
-            [clojre.string :as string]
+            [clojure.string :as string]
             [clojure.edn :as edn]))
 
 (def test-input
@@ -10,3 +10,21 @@
    "up 3"
    "down 8"
    "forward 2"])
+
+(defn parse-command
+  [command]
+  (let [[direction amount] (string/split command #" ")]
+    [(keyword direction) (edn/read-string amount)]))
+
+(defn execute-command
+  [[position depth] [direction amount]]
+  (case direction
+    :forward [(+ position amount) depth]
+    :up [position (- depth amount)]
+    :down [position (+ depth amount)]))
+
+(defn part1
+  []
+  (let [command-lines (slurp-lines "input2.txt")
+        commands (map parse-command command-lines)]
+    (apply * (reduce execute-command [0 0] commands))))
