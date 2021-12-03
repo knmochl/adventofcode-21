64efa7766b28dbcceb58bc399297a00a9709f424
Nathan Sullivan <nsullivan@netsuite.com>
2021-12-02 21:02:15 -0800

    Add test-input for Day 3

---
+++ src/advent/day3.clj
@@ -0,0 +1,18 @@
+(ns advent.day3
+  (:require [advent.util :refer [slurp-lines]]
+            [clojure.string :as string]
+            [clojure.edn :as edn]))
+
+(def test-input
+  ["00100"
+   "11110"
+   "10110"
+   "10111"
+   "10101"
+   "01111"
+   "00111"
+   "11100"
+   "10000"
+   "11001"
+   "00010"
+   "01010"])
