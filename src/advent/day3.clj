7b40b246840e89380bf4c108c1796ca35c70b42a
Nathan Sullivan <nsullivan@netsuite.com>
2021-12-02 21:57:07 -0800

    Add solution for Day 3 Part 1

--- src/advent/day3.clj
+++ src/advent/day3.clj
@@ -16,3 +16,41 @@
    "11001"
    "00010"
    "01010"])
+
+(defn get-entry
+  [item n]
+  (nth item n))
+
+(defn get-sequence
+  [items n]
+  (map #(get-entry %1 n) items))
+
+(defn most-common-digit
+  [digits]
+  (let [freqs (frequencies digits)]
+    (if (< (freqs \0) (freqs \1))
+      \1
+      \0)))
+
+(defn least-common-digit
+  [digits]
+  (let [freqs (frequencies digits)]
+    (if (> (freqs \0) (freqs \1))
+      \1
+      \0)))
+
+(defn calculate-digit
+  [items f n]
+  (f (get-sequence items n)))
+
+(defn convert-binary
+  [digits]
+  (reduce #(+ (* 2 %1) %2)
+          (map (comp edn/read-string str) digits)))
+
+(defn part1
+  []
+  (let [values (slurp-lines "input3.txt")
+        gamma (convert-binary (map (partial calculate-digit values most-common-digit) (range (count (first values)))))
+        epsilon (convert-binary (map (partial calculate-digit values least-common-digit) (range (count (first values)))))]
+    (* gamma epsilon)))
